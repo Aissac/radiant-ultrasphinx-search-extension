c1aef66af0ccc7385837b0daf6bb21b6e57eb8d8
Cristi Duma <cristi.duma@aissac.ro>
2009-02-10 14:14:08 +0200

    finished the search page type

--- app/models/ultrasphinx_results_page.rb
+++ app/models/ultrasphinx_results_page.rb
@@ -15,27 +15,40 @@ class UltrasphinxResultsPage < Page
   
   def ultrasphinx_get_results(request, response)
     
-    @query = "post"
+    @query = request.parameters[:query] # should it be safe? sanitize, maybe strip?
     
-    Ultrasphinx::Search.excerpting_options = HashWithIndifferentAccess.new({
-    :before_match => '<span style="background-color: yellow;">',
-    :after_match => '</span>',
-    :chunk_separator => "...",
-    :limit => 256,
-    :around => 3,
-    :content_methods => ['title', 'content']
-    })
+    page = Page.find_by_slug(request.parameters[:url])
+    @config = configure(page)
     
-    @search = Ultrasphinx::Search.new(:query => @query)
-    # @search.run
-    @search.excerpt
+    config_excerpting_options(@config) if excerpt?
+    
+    @search = Ultrasphinx::Search.new(
+      :per_page => @config[:per_page],
+      :query => @query,
+      :class_names => "Page",
+      :weights => {'title' => 10.0}
+      )
+    excerpt? ? @search.excerpt : @search.run
     @results = @search.results
   end
   
+  desc %{
+    Gives access to the search results.
+    
+    *Usage:*
+    <pre><code><r:results>...</r:results></code></pre>
+  }
   tag 'results' do |tag|
     tag.locals.results = tag.locals.page.results
     tag.expand
   end
+  
+  desc %{
+    Cycles through each of the search results. Inside this tag the page attributes are mapped to the current result.
+    
+    *Usage:*
+    <pre><code><r:results:each>...</r:results:each></code></pre>
+  }
   
   tag 'results:each' do |tag|
     result = []
@@ -44,5 +57,37 @@ class UltrasphinxResultsPage < Page
       result << tag.expand
     end
     result
-  end  
+  end
+  
+  desc %{
+    Presents the excerpted and highlighted content
+    
+    *Usage:*
+    <pre><code><r:results:excerpt_content /></code></pre>
+  }
+  
+  tag 'results:excerpt_content' do |tag|
+    tag.locals.page.page_part_content
+  end
+  
+  private
+    def configure(page)
+      string = page.render_part(:sphinx)
+      string.empty? ? {} : YAML::load(string).symbolize_keys
+    end
+    
+    def excerpt?
+      @config[:excerpt] || @config[:excerpt].nil?
+    end
+    
+    def config_excerpting_options(config)
+      Ultrasphinx::Search.excerpting_options = HashWithIndifferentAccess.new({
+        :before_match => config[:before_match],
+        :after_match => config[:after_match],
+        :chunk_separator => config[:chunk_separator],
+        :limit => config[:limit],
+        :around => config[:around],
+        :content_methods => [['page_part_content'], ['title']]
+      })
+    end
 end
--- lib/ultrasphinx_search/page_extensions.rb
+++ lib/ultrasphinx_search/page_extensions.rb
@@ -2,8 +2,20 @@ module UltrasphinxSearch
   module PageExtensions
     def self.included(base)
       base.class_eval do
-        is_indexed :fields => ["title"]
+        is_indexed :fields => ["title"],
+                   :concatenate => [{:association_name => 'parts', :field => 'content', :as => 'page_part_content'}],
+                   :delta => true
       end
+      
+      base.send(:include, InstanceMethods)
+   end
+   
+   module InstanceMethods
+     def page_part_content
+       @page_part_content ||= parts.map { |part|
+         part.filter.filter(part.content)
+       }.join(' ')
+     end
    end
   end
 end
